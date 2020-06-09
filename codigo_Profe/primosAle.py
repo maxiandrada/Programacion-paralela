19c3329a20d3750acd6e37643f07a775a23e2566
root <root@host001.hpc.local>
2020-06-09 03:06:08 -0300

    cambiando nombre

--- codigo_Profe/primosAle.py
+++ codigo_Profe/primosAle.py
@@ -4,9 +4,13 @@ from mpi4py import MPI
 import numpy as np
 
 def esPrimo(n):
-  if n % 2 == 0:
+  if n < 2 == 0:
     return False
-
+  if n == 2:
+    return True
+  if n%2 == 2:
+    return False
+  
   sqrt_n = int(math.floor(math.sqrt(n)))
     
   for i in range(3, sqrt_n + 1, 2):
@@ -17,8 +21,8 @@ def esPrimo(n):
 
 def main1():
   startTime=time()
-  for i in range(100000):
-    print (str(i)+" es primo\n" if esPrimo(i) else str(i)+" no es primo\n")
+  for i in range(1000):
+    print (str(i)+" es primo")
       
   print ("CPU time " + str(time()-startTime))
 
@@ -26,17 +30,22 @@ def main2():
   comm = MPI.COMM_WORLD
   rank = comm.Get_rank()
   nproc = comm.Get_size()
+  cantidad = 0
   if rank==0:
     l=np.arange(10,dtype='i')
     for i in range(1,nproc):
-      comm.Send([l[i*int(len(l)/nproc):(i+1)*int(len(l)/nproc)], MPI.INT], dest=i)
+      comm.Send([l[i*int(len(l)/nproc):(i+1)*int(len(l)/nproc)] , MPI.INT], dest=i)
     for i in l[:int(len(l)/nproc)]:
-      print (str(i)+" es primo\n" if esPrimo(i) else str(i)+" no es primo\n")
+      if esPrimo(i):
+        cantidad+=1
   else:
+    l = np.zeros(maximo, dtype='i')
     comm.Recv([l, MPI.INT], source=0)
     for i in l:
-      print (str(i)+" es primo\n" if esPrimo(i) else str(i)+" no es primo\n")
+      if(esPrimo(i)):
+        cantidad+=1
+    print("rank "+ str(rank) + " tiene "+str(cant))
 
 if __name__ == '__main__':
-  #main1()
-  main2()
+  main1()
+  #main2()
--- rm_nodos.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-pass="sebi0495"
-#Se pasa por parametro la ruta del archivo a eliminar o el directorio a eliminar
-
-sshpass -p $pass ssh root@192.168.10.101 'rm -R -f' $1 '|| true'
-sshpass -p $pass ssh root@192.168.10.102 'rm -R -f' $1 '|| true'
-sshpass -p $pass ssh root@192.168.10.103 'rm -R -f' $1 '|| true'
-echo borrado exitosamente de los nodos 1, 2 y 3
