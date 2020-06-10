b70e80a786f5827ac8ef2a30fb463c43193f3b09
alumno <alumno@server.hpc.local>
2020-06-10 11:02:39 -0300

    mejorado script scp

--- scpToNodes.sh
+++ scpToNodes.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-scp $1 alumno@192.168.10.101:/datos 
+ip1=192.168.160.101
+ip2=192.168.160.102
+ip3=192.168.160.103
+usuario="alumno"
+ssh $usuario@$ip1 mkdir -p $PWD && scp $1 $usuario@$ip1:$PWD
 echo "Se copió al nodo 1"
-scp $1 alumno@192.168.10.102:/datos
+ssh $usuario@$ip2 mkdir -p $PWD && scp $1 $usuario@$ip2:$PWD
 echo "Se copió al nodo 2"
-scp $1 alumno@192.168.10.103:/datos
+ssh $usuario@$ip3 mkdir -p $PWD && scp $1 $usuario@$ip3:$PWD
 echo "Se copió al nodo 3"
