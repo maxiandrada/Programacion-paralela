ec188f641b126b557238c0bb84de503477c8ade7
root <root@host001.hpc.local>
2020-06-11 12:08:21 -0300

    .

--- primos_paralelo.py
+++ primos_paralelo.py
@@ -19,7 +19,7 @@ def algoritmoSecuencial(MAX):
   startTime=time()
   cantidad = 1  #Considera al nro 2 como primer primo
   maximo = MAX
-  for i in range(3,maximo,2):
+  for i in range(3,maximo,1):
     if(esPrimo(i)):
       cantidad+=1
 
@@ -33,7 +33,7 @@ if __name__ == '__main__':
   rank = comm.Get_rank()
   nproc = comm.Get_size()
   maximo = 500000
-  cantidad = 1    #Considera al nro 2 como primero primo
+  cantidad = 0
   cantidadSecuencial = 0
   tiempoParalelo = 0
   tiempoSecuencial = 0
@@ -48,10 +48,10 @@ if __name__ == '__main__':
         indiceSuperior = (i+1)*int(maximo/nproc)
       comm.send(indiceInferior, dest=i,tag=1)
       comm.send(indiceSuperior, dest=i, tag=2)
-    for i in range(3,1*int(maximo/nproc),2):
+    for i in range(1,1*int(maximo/nproc),2):
       if esPrimo(i):
         cantidad+=1 
-
+    
     tiempoParalelo=time()-startTime
     print ("CPU time paralelo " + str(tiempoParalelo))
     
@@ -64,10 +64,10 @@ if __name__ == '__main__':
     
     if(indiceInferior%2==0):
       indiceInferior+=1
-    for i in range(indiceInferior,indiceSuperior,2):
+    for i in range(indiceInferior,indiceSuperior):
       if(esPrimo(i)):
         cantidad +=1 
-    #print("rank "+ str(rank)+ " tiene " + str(cantidad))
+    
   data = comm.gather(cantidad, root=0)
   
   if(data!=None):
@@ -81,6 +81,4 @@ if __name__ == '__main__':
       else:
         porcentaje =(1 - float(tiempoParalelo/tiempoSecuencial))*100
         print("El algortimo paralelo es "+str(porcentaje)+"% mas rapido que el secuencial")
-
-
 
