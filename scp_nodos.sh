33e6646e5417b95cfdaa9b9e778784edec222a49
root <root@host001.hpc.local>
2020-06-11 02:52:46 -0300

    .

--- scp_nodos.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#Se pasa por parametro el archivo o directorio que se quiere copiar a los nodos y los copia en el
-#segundo parametro
-psswd="sebi0495"
-sshpass -p $psswd scp -r $1 root@192.168.10.101:$2$1
-sshpass -p $psswd scp -r $1 root@192.168.10.102:$2$1
-sshpass -p $psswd scp -r $1 root@192.168.10.103:$2$1
-echo Copiado exitosamente en los nodos 1,2 y 3
